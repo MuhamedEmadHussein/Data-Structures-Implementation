b95d25af05cf29e687386f4f219ea8e58ed5140b
MuhamedEmadHussein <127058766+MuhamedEmadHussein@users.noreply.github.com>
2023-03-11 04:36:35 +0200

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,306 @@
+from collections import deque
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.right_child = None
+        self.left_child = None
+
+
+n1 = Node("root node")
+n2 = Node("left child node")
+n3 = Node("right child node")
+n4 = Node("left grandchild node")
+
+n1.left_child = n2
+n1.right_child = n3
+n2.left_child = n4
+
+# current = n1
+# while current:
+#     print(current.data)
+#     current = current.left_child
+
+
+def inorder(root_node):
+
+    current = root_node
+
+    if current is None:
+        return
+
+    inorder(current.left_child)
+    print(current.data)
+    inorder(current.right_child)
+
+
+# inorder(n1)
+
+def preorder(root_node):
+    current = root_node
+    if current is None:
+        return
+    print(current.data)
+    preorder(current.left_child)
+    preorder(current.right_child)
+
+
+# preorder(n1)
+
+def postorder(root_node):
+    current = root_node
+    if current is None:
+        return
+    postorder(current.left_child)
+    postorder(current.right_child)
+    print(current.data)
+
+
+# postorder(n1)
+#### LEVEL ORDER TRAVERSAL #######################################################
+
+
+def level_order_traversal(root_node):
+    list_nodes = []
+    queue_list = deque([root_node])
+
+    while len(queue_list) > 0:
+
+        node = queue_list.popleft()
+        list_nodes.append(node.data)
+
+        if node.left_child:
+            queue_list.append(node.left_child)
+
+            if node.right_child:
+                queue_list.append(node.right_child)
+
+    return list_nodes
+
+
+print(level_order_traversal(n1))
+
+#  ####################################################################################
+
+
+class TreeNode:
+    def __init__(self, data=None):
+        self.data = data
+        self.right = None
+        self.left = None
+
+
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def push(self, item):
+        self.elements.append(item)
+
+    def pop(self):
+        return self.elements.pop()
+
+
+expr = "4 5 + 5 3 - *".split()
+stack = Stack()
+
+for term in expr:
+
+    if term in "+*-/":
+        node = TreeNode(term)
+        node.right = stack.pop()
+        node.left = stack.pop()
+
+    else:
+        node = TreeNode(int(term))
+
+    stack.push(node)
+
+
+def calc(node):
+
+    if node.data == "+":
+        return calc(node.left) + calc(node.right)
+    elif node.data == "-":
+        return calc(node.left) - calc(node.right)
+    elif node.data == "*":
+        return calc(node.left) * calc(node.right)
+    elif node.data == "/":
+        return calc(node.left) / calc(node.right)
+    else:
+        return node.data
+
+
+root = stack.pop()
+print(calc(node=root))
+
+######### BST ######################################################################
+
+
+class Tree:  # BST
+
+    def __init__(self):
+        self.root_node = None
+
+    def insert(self, data):
+
+        node = Node(data=data)
+
+        if self.root_node is None:
+            self.root_node = node
+            return self.root_node
+
+        else:
+
+            current = self.root_node
+            parent = None
+
+            while True:
+                parent = current
+                if node.data < current.data:
+                    current = current.left_child
+                    if current is None:
+                        parent.left_child = node
+                        return self.root_node
+
+                else:
+                    current = current.right_child
+                    if current is None:
+                        parent.right_child = node
+                        return self.root_node
+
+    def search(self, data):
+        current = self.root_node
+
+        while True:
+
+            if current is None:
+                print("Item Not Found")
+                return None
+
+            else:
+                if current.data == data:
+                    print("Item Found " + str(data))
+                    return data
+
+                elif data < current.data:
+                    current = current.left_child
+
+                else:
+                    current = current.right_child
+
+    # DELETION
+    def get_node_with_parent(self, data):
+        parent = None
+        current = self.root_node
+
+        if current is None:
+            return (parent, None)
+
+        while True:
+
+            if current.data == data:
+                return (parent, current)
+
+            elif data < current.data:
+                parent = current
+                current = current.left_child
+
+            else:
+                parent = current
+                current = current.right_child
+
+        return (parent, current)
+
+    def remove(self, data):
+
+        parent, node = self.get_node_with_parent(data)
+
+        if parent is None and node is None:
+            return False
+
+        # Get children count
+
+        children_count = 0
+        if node.left_child and node.right_child:
+            children_count = 2
+
+        elif (node.left_child is None) and (node.right_child is None):
+            children_count = 0
+
+        else:
+            children_count = 1
+        ############################
+        # Scenario #1
+        if children_count == 0:
+
+            if parent:
+
+                if parent.right_child is node:
+                    parent.right_child = None
+
+                else:
+                    parent.left_child = None
+
+            else:
+                self.root_node = None
+
+        # Scenario #2
+        if children_count == 1:
+            next_node = None
+
+            if node.left_child:
+                next_node = node.left_child
+            else:
+                next_node = node.right_child
+
+            if parent:
+
+                if parent.left_child is node:
+                    parent.left_child = next_node
+
+                else:
+                    parent.right_child = next_node
+
+            else:
+                self.root_node = next_node
+
+        # SCENARIO #3
+        else:
+
+            parent_of_leftmost_node = node
+            leftmost_node = node.right_child
+            while leftmost_node.left_child:
+                parent_of_leftmost_node = leftmost_node
+                leftmost_node = leftmost_node.left_child
+
+            node.data = leftmost_node.data
+
+            if parent_of_leftmost_node.left_child == leftmost_node:
+                parent_of_leftmost_node.left_child = leftmost_node.right_child
+            else:
+                parent_of_leftmost_node.right_child = leftmost_node.right_child
+
+    def find_min(self):
+        current = self.root_node
+        while current.left_child:
+            current = current.left_child
+        return current.data
+
+    def find_max(self):
+        current = self.root_node
+        while current.right_child:
+            current = current.right_child
+        return current.data
+
+
+tree = Tree()
+r = tree.insert(5)
+r = tree.insert(2)
+r = tree.insert(7)
+r = tree.insert(9)
+r = tree.insert(1)
+inorder(tree.root_node)
+tree.search(6)
